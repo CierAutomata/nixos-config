181c2167061597abf9d2831419062ce3f3bc1faf
Cier <CierAutomata@pm.me>
2026-04-17 08:59:45 +0200

    sops deaktiviert. Rebuild test

--- modules/sops.nix
+++ modules/sops.nix
@@ -10,9 +10,13 @@
       generateKey = false;
     };
 
-    # Passwort-Secret aktivieren sobald hashedPassword in secrets.yaml gepflegt ist:
-    #secrets.user-password = {
-    #  neededForUsers = true;
-    #};
+    # Um das Passwort aus secrets.yaml zu nutzen, beides auskommentieren:
+    # 1) Hier den richtigen YAML-Pfad angeben:
+    # secrets.user-password = {
+    #   key = "users/${config.myConfig.userName}/hashedPassword";
+    #   neededForUsers = true;
+    # };
+    # 2) In hosts/<host>/configuration.nix ergänzen:
+    # users.users.<name>.hashedPasswordFile = config.sops.secrets.user-password.path;
   };
 }
